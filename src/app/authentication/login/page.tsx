4812a1716054f990fa891f5c76fbef07c79de180
Nawawit <nawawit.p@clicknext.com>
2025-05-13 10:08:16 +0700

    Update page.tsx

--- src/app/authentication/login/page.tsx
+++ src/app/authentication/login/page.tsx
@@ -55,7 +55,7 @@ const Login2 = () => {
                     color="textSecondary"
                     mb={1}
                   >
-                    Your Social Campaigns 222
+                    Your Social Campaigns 113
                   </Typography>
                 }
                 subtitle={
